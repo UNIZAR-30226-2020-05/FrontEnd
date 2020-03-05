1566e64b45c35cb8999dae1d5249330313f2dace
samuelosb <samuelosb@users.noreply.github.com>
2020-03-06 00:46:44 +0100

    Actualizado componente reproductor
    Con esquema de UI

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ReproductorComponent } from './reproductor/reproductor.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ReproductorComponent
   ],
   imports: [
     BrowserModule
