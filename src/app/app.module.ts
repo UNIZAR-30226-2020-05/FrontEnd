dfbad67f33be0c742704fe4f11c7ffd702f1da62
SalomeReav <salome.reav.50@gmail.com>
2020-03-19 17:46:53 +0100

    act

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,6 @@ import { AppComponent } from './app.component';
 import { ReproductorComponent } from './reproductor/reproductor.component';
 import { PanelSocialComponent } from './panel-social/panel-social.component';
 import { PanelListasComponent } from './panel-listas/panel-listas.component';
-import { BarraBusquedaComponent } from './barra-busqueda/barra-busqueda.component';
 
 @NgModule({
   declarations: [
@@ -13,7 +12,6 @@ import { BarraBusquedaComponent } from './barra-busqueda/barra-busqueda.componen
     ReproductorComponent,
     PanelSocialComponent,
     PanelListasComponent,
-    BarraBusquedaComponent
   ],
   imports: [
     BrowserModule
