2be654bee1c334c99b254463b9254d5b52f6c9a7
samuelosb <samuelosb@users.noreply.github.com>
2020-03-26 19:49:03 +0100

    Mejoras registro de usuario

--- src/app/registro/registro.component.ts
+++ src/app/registro/registro.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit,Input} from '@angular/core';
+import { Component, OnInit, Input} from '@angular/core';
 
 @Component({
   selector: 'app-registro',
@@ -8,18 +8,26 @@ import { Component, OnInit,Input} from '@angular/core';
 export class RegistroComponent implements OnInit {
   @Input() registro1; //Flag si se solicita proceso de registro
   public registro2; //Flag si está en el paso dos de registro.
+  public registro3; //Flag si está en el paso tres de registro.
 
+  aceptadoT;
   imageSeleccion;
   constructor() { }
 
   ngOnInit(): void {
-    this.registro1=false;
-    this.registro2=false;
-    this.imageSeleccion=0;
+    this.registro1 = false;
+    this.registro2 = false;
+    this.registro3 = false;
+    this.aceptadoT = false;
+    this.imageSeleccion = 0;
   }
 
   receiveMessageChild($event){
-    this.registro1=($event)
+    this.registro1=($event);
   }
 
+  alternarAceptado(){
+    if (this.aceptadoT) { this.aceptadoT = false;}
+    else this.aceptadoT = true;
+  }
 }
