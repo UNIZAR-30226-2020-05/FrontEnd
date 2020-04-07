035fce01d574ed532ae9ef6f6e25f6e42839093a
samuelosb <samuelosb@users.noreply.github.com>
2020-04-07 18:58:27 +0200

    Mejoras vista album y contenido

--- src/app/vista-album/vista-album.component.ts
+++ src/app/vista-album/vista-album.component.ts
@@ -7,6 +7,12 @@ import {Component, Input, OnInit} from '@angular/core';
 })
 export class VistaAlbumComponent implements OnInit {
 
+  lista = ['Primo Victoria', 'Reign of Terror', 'Wolfpack','Counterstrike', 'Stalingrad', 'Into the Fire',
+    'Purple Heart', 'Metal Machine','Counterstrike 2', 'Metal Machine','Counterstrike 2','Counterstrike 2', 'Metal Machine'];
+
+  //
+  contador: number = 0;
+  artistaAlbum: string = 'Sabaton'
   constructor() { }
   show = false;
   ngOnInit(): void {
