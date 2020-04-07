6466ba49e9a1f9ef70f1bedd45c2d58a0a5af41f
SalomeReav <salome.reav.50@gmail.com>
2020-04-07 17:44:07 +0200

    pantalla de favorito, y botones que cambian de una a otra. Modificados algunos parametros anteriores.

--- src/app/panel-listas/panel-listas.component.ts
+++ src/app/panel-listas/panel-listas.component.ts
@@ -8,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class PanelListasComponent implements OnInit {
 
   mostrarCrear:boolean=false;
+  mostrarFav:boolean=false;
   constructor() { }
 
   ngOnInit(): void {
