fdd86abb73dd08802e32612abe88135efad0d5fc
samuelosb <samuelosb@users.noreply.github.com>
2020-04-12 19:08:01 +0200

    Mejoras busqueda de amigos

--- src/app/panel-social/panel-social.component.ts
+++ src/app/panel-social/panel-social.component.ts
@@ -1,5 +1,7 @@
 import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
-import {HttpClient, HttpParams, HttpClientModule} from '@angular/common/http';
+import {HttpClient, HttpParams, HttpClientModule, HttpResponse, HttpErrorResponse} from '@angular/common/http';
+import {User} from '../app.component';
+import {error} from '@angular/compiler/src/util';
 
 @Component({
   selector: 'app-panel-social',
@@ -9,19 +11,25 @@ import {HttpClient, HttpParams, HttpClientModule} from '@angular/common/http';
 export class PanelSocialComponent implements OnInit {
 
   public URL_API = 'http://localhost:8080';
+
+
   editarUsuario = true;
   @Output() messageEvent = new EventEmitter<boolean>();
-
 
   listaVacia: boolean;
   mostarBusquedaAmigos: boolean;
   nickBusca: string;
+  busqIniciada: boolean;
+  noEncuentra: boolean;
+  usuarioLogeado: User;
+  usuario: User;
 
   constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
     this.listaVacia = false;
     this.mostarBusquedaAmigos = false;
+    this.busqIniciada = false;
   }
 
   activarBusqueda() {
@@ -32,16 +40,27 @@ export class PanelSocialComponent implements OnInit {
     this.mostarBusquedaAmigos = false;
   }
 
-  sendMessageFather() {
-    this.messageEvent.emit(this.editarUsuario);
-  }
-
   buscarAmigo() {
     const params = new HttpParams()
       .set('nick', this.nickBusca);
-    return this.http.get(this.URL_API + '/user/get', {params}).subscribe(
-      (resp: string) => { console.log(resp); } );
 
+    this.http.get(this.URL_API + '/user/get', {params})
+      .subscribe(
+        (resp: User) => { this.busqIniciada = true; this.noEncuentra = false; this.usuario = resp; console.log(resp.nick); },
+        (erroro: string) => {this.busqIniciada = true; this.noEncuentra = true; }
+      );
+  }
+
+  agregarAmigo() {
+    const params = new HttpParams()
+      .set('id2', this.usuario.id.toString());
+
+    this.http.patch(this.URL_API + '/user/addAmigo{id1}', {params})
+      .subscribe((resp: User) => { this.usuario = resp; console.log(resp.nick); } );
+  }
+
+  sendMessageFather() {
+    this.messageEvent.emit(this.editarUsuario);
   }
 
 }
