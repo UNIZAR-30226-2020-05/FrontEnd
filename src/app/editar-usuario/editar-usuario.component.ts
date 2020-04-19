cfba9b5b0667835f2a6ec81e386efaf99edac0a6
DaniRoype <dani.roype@gmail.com>
2020-04-19 20:27:54 +0200

    funcionalidad borrado de usuario

--- src/app/editar-usuario/editar-usuario.component.ts
+++ src/app/editar-usuario/editar-usuario.component.ts
@@ -1,5 +1,6 @@
 import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
 import {HttpClient, HttpParams, HttpClientModule} from '@angular/common/http';
+import {ServicioComponentesService} from '../servicios/servicio-componentes.service';
 
 @Component({
   selector: 'app-editar-usuario',
@@ -7,8 +8,13 @@ import {HttpClient, HttpParams, HttpClientModule} from '@angular/common/http';
   styleUrls: ['./editar-usuario.component.css']
 })
 export class EditarUsuarioComponent implements OnInit {
-  @Input() mostrar;
+  @Input() mostrar; // Muestra la pantalla de cambio de contraseña
+  public aBorrar; // Muestra la pantalla de confirmación de eliminación de cuenta
+  public passCambiada; // Muestra el mensaje de información de cambio de password
   public imageSeleccion;
+  public login: true;
+
+  @Output() messageEvent = new EventEmitter<boolean>();
 
   public URL_API = 'http://localhost:8080';
 
@@ -16,34 +22,57 @@ export class EditarUsuarioComponent implements OnInit {
   passN: string;
   passN2: string;
   id: number;
-  constructor(private http: HttpClient) {
+  constructor(private http: HttpClient, private Servicio: ServicioComponentesService) {
     this.passN = '';
     this.passN2 = '';
-    this.id = 5;
+    this.id = 11;
   }
-
+  // Se muestra al pulsar en el engranaje de panel social
   receiveMessageChild($event) {
     this.mostrar = ($event);
   }
-
+  // Cancelar
   cancelarEdit() {
     this.mostrar = false;
+    this.aBorrar = false;
+    this.passCambiada = false;
   }
+  // Para mostrar la pantalla de borrado de usuario
   borrarUsuario() {
+    this.mostrar = false;
+    this.aBorrar = true;
+  }
+  // Cancela el borrado
+  cancelaBorrado() {
+    this.mostrar = true;
+    this.aBorrar = false;
+  }
+  // Borra el usuario y vuelve a la pantalla de login
+  confirmaBorrado() {
     this.http.delete(this.URL_API + '/user/delete/' + this.id).subscribe(
       (resp: string) => { console.log(resp); } );
+    this.Servicio.nextMessage2(this.login);
     this.mostrar = false;
+    this.aBorrar = false;
   }
+  // Comprueba que la contraseña se haya repetido correctamente
   contrasenaBien() {
     return (this.passN !== '' && this.passN2 !== '' && this.passN === this.passN2);
   }
+  // Cambia la contraseña del usuario
   cambio() {
     this.http.patch(this.URL_API + '/user/modifyPass/' + this.id, btoa(this.passN)).subscribe(
       (resp: string) => { console.log(resp); } );
     this.mostrar = false;
+    this.passCambiada = true;
   }
-
+  // Cambia a la pantalla de información de cambio de contraseña
+  finalCambio() {
+    this.passCambiada = false;
+  }
   ngOnInit(): void {
+    this.mostrar = false;
+    this.aBorrar = false;
+    this.passCambiada = false;
   }
-
 }
