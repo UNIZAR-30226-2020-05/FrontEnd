7943f7e4d26b4200073ec2b6a0d9d27c3e25c98e
DaniRoype <dani.roype@gmail.com>
2020-04-19 13:36:43 +0200

    funcionalidad cambio contra

--- src/app/editar-usuario/editar-usuario.component.ts
+++ src/app/editar-usuario/editar-usuario.component.ts
@@ -1,4 +1,5 @@
 import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+import {HttpClient, HttpParams, HttpClientModule} from '@angular/common/http';
 
 @Component({
   selector: 'app-editar-usuario',
@@ -9,7 +10,17 @@ export class EditarUsuarioComponent implements OnInit {
   @Input() mostrar;
   public imageSeleccion;
 
-  constructor() { }
+  public URL_API = 'http://localhost:8080';
+
+  // Atributo a cambiar
+  passN: string;
+  passN2: string;
+  id: number;
+  constructor(private http: HttpClient) {
+    this.passN = '';
+    this.passN2 = '';
+    this.id = 5;
+  }
 
   receiveMessageChild($event) {
     this.mostrar = ($event);
@@ -18,9 +29,20 @@ export class EditarUsuarioComponent implements OnInit {
   cancelarEdit() {
     this.mostrar = false;
   }
-  finalizarEdit() {
+  borrarUsuario() {
+    this.http.delete(this.URL_API + '/user/delete/' + this.id).subscribe(
+      (resp: string) => { console.log(resp); } );
     this.mostrar = false;
   }
+  contrasenaBien() {
+    return (this.passN !== '' && this.passN2 !== '' && this.passN === this.passN2);
+  }
+  cambio() {
+    this.http.patch(this.URL_API + '/user/modifyPass/' + this.id, btoa(this.passN)).subscribe(
+      (resp: string) => { console.log(resp); } );
+    this.mostrar = false;
+  }
+
   ngOnInit(): void {
   }
 
