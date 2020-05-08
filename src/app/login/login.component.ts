0cad327375e9d387254dcbcbe99adac9e8bcd8f2
samuelosb <samuelosb@users.noreply.github.com>
2020-05-08 23:59:09 +0200

    Objeto usuario logeado se inicializa desde el principio

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,7 +1,7 @@
 import {Component, Output, EventEmitter, OnInit, Input} from '@angular/core';
 import {HttpClient, HttpParams, HttpClientModule} from '@angular/common/http';
 import {User, UserRequest} from '../app.component';
-import {ServicioComponentesService} from "../servicios/servicio-componentes.service";
+import {ServicioComponentesService} from '../servicios/servicio-componentes.service';
 
 @Component({
   selector: 'app-login',
@@ -10,15 +10,16 @@ import {ServicioComponentesService} from "../servicios/servicio-componentes.serv
 })
 export class LoginComponent implements OnInit {
   logeado;
-  registro:boolean=true;
-  aviso:boolean=false;
+  registro: boolean = true;
+  aviso: boolean = false;
 
   // campos de login
-  nomUsuario:string;
-  contrasena:string;
-  correctoNick:string;
+  nomUsuario: string;
+  contrasena: string;
+  correctoNick: string;
   correctoPass: string;
 
+  usuario: User = new User();
 
 
   @Output() messageEvent = new EventEmitter<boolean>();
@@ -38,7 +39,7 @@ export class LoginComponent implements OnInit {
     this.messageEvent.emit(this.registro);
   }
 
-  usuario:User;
+
   comprobar(){
     const params= new HttpParams().set('nick',this.nomUsuario).set('pass',btoa(this.contrasena));
     this.http.get(this.Servicio.URL_API + '/user/logIn',{params}).subscribe(
