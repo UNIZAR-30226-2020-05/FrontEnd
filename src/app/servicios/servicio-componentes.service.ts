48f62b644104b5e707a85723f9b565d9e6709af5
SalomeReav <salome.reav.50@gmail.com>
2020-05-21 01:30:40 +0200

    Modificaciones varias y correciones de elementos descritos en el issue.

--- src/app/servicios/servicio-componentes.service.ts
+++ src/app/servicios/servicio-componentes.service.ts
@@ -151,7 +151,7 @@ export class ServicioComponentesService {
   sharedMessageObjPodcast = this.messageObjetoPodcast.asObservable();
 
   constructor(private http: HttpClient) {
-    this.URL_API = 'http://3.22.247.114:8080';
+    this.URL_API = 'https://3.18.169.143:8443';
     this.nuevo = new User();
     this.objLista = new ListaCancion();
     this.albumActiv = new Album();
