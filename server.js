a0daab4d7221ca4256f1bce5bc2e51171f0d793c
patseho <juanv4004@gmail.com>
2020-05-21 11:44:58 +0200

    use cors-anywhere (server.js)

--- server.js
+++ server.js
@@ -1,15 +1,9 @@
 //Install express server
 const express = require('express');
 const path = require('path');
-const cors = require('cors');
+const cors_proxy = require('cors-anywhere');
 
 const app = express();
-
-app.use(cors());
-/*app.use(function(req,res,next){
-  res.header("Acces-Control-Allow-Origin", '*');
-  next();
-});*/
 
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist/FrontEnd'));
@@ -17,6 +11,17 @@ app.use(express.static(__dirname + '/dist/FrontEnd'));
 app.get('/*', function(req,res) {
   res.sendFile(path.join(__dirname+'/dist/FrontEnd/index.html'));
 });
+
+/*app.use(function(req,res,next){
+  res.header("Acces-Control-Allow-Origin", '*');
+  next();
+});*/
+app.use(cors_proxy.createServer(
+  {
+    originWhitelist: [],
+    requireHeader: ['origin', 'x-requested-with'],
+    removeHeaders: ['cookie', 'cookie2']
+  }));
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
